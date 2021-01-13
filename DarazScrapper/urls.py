6e1f32f3a875658dff00b3b56ca0781e83d93bdd
unknown <aliahmad538404@gmail.com>
2021-01-14 01:41:17 +0500

    Final Web Application

--- DarazScrapper/urls.py
+++ DarazScrapper/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 D = views.Scrap()
 
 urlpatterns = [
-    path('',D.Daraz, name='Home'),
+    path('', D.Daraz, name='Home'),
+    path('About', D.About, name='About'),
+    path('Help', D.Help, name='Help'),
 ]
--- DarazScrapper/views.py
+++ DarazScrapper/views.py
@@ -4,18 +4,17 @@ import requests
 import datetime
 import json
 from bs4 import BeautifulSoup
-
+import math
 
 # Create your views here.
 class Scrap:
     def Daraz(self,request):
         if request.method=='POST':
             Urls = request.POST['URL']
-            Url=Urls.split(',')
-            URL_foud = True
+            Url = Urls.split(',')
             A = []
             try:
-                A.append(Scrap.execute_Scrap(Url, URL_foud))
+                A.append(Scrap.execute_Scrap(Url))
             except:
                 messages.error(request,'Error Check You URL Or Input Comma Separated URLS')
                 return render(request,'DarazScrapper/Index.html')
@@ -23,26 +22,49 @@ class Scrap:
             return render(request,'DarazScrapper/Index.html',Data)
         return render(request,'DarazScrapper/Index.html')
 
-
     @classmethod
-    def execute_Scrap(cls,URL,URL_foud):
+    def execute_Scrap(cls, URL):
         try:
             result = []
             for i in URL:
                 r = requests.get(i)
                 soup = BeautifulSoup(r.text, 'html.parser')
-                scripts = str(soup.find_all("script")[143])
+                scripts = str(soup.find_all("script")[142])
                 stock_s = scripts.split('stockList')[1].strip().split(']')[0][3:]
                 stock_list = json.loads(stock_s)
                 stock = int(stock_list['stoock'])
                 product_name = scripts.split('pdt_name')[1].split('page')[0][2:-2]
+                price = scripts.split('pdt_price')[1].split('"}')[0][6:]
                 date = datetime.date.today()
-                # print('---------------',product_name, stock, date)
-                print(stock_s)
-                A = [product_name, stock, date]
+                r = scripts.split('ratings')[4][3:].split('}')[0].split(',')
+                r1 = r[0]+r[1]
+                r2 = scripts.split('ratings')[4][3:].split('}')[0].split(':')[4][1:-1].split(',')
+                r3 = f'Ratings 5-stars:{r2[0]}\t 4-stars:{r2[1]}\t3-stars:{r2[2]}\t2-stars:{r2[3]}\t1-stars:{r2[4]}\t'
+                rating = r1 + '\n' + r3
+                discount, discounted_price = Scrap.discount(scripts, price)
+                A = [product_name, stock, price, rating, discount, discounted_price, date]
                 result.append(A)
+
             return result
+        except Exception as e:
+            raise Exception(e)
+
+    @classmethod
+    def discount(cls, script,price):
+        try:
+            stock_s = script.split('pdt_discount')[1].split(',')[0][4:-2]
+            discount = stock_s
+            d = 100 - int(stock_s)
+            discount_price = math.ceil(int(price)*(d/100))
+
         except:
-            raise Exception('')
+            discount = 0
+            discount_price = price
+        return discount, discount_price
 
 
+    def Help(self, request):
+        return render(request, 'DarazScrapper/Help.html')
+
+    def About(self, request):
+        return render(request, 'DarazScrapper/About.html')
