a4cc27a8836a524abadbff4f77ac70a72be470e8
unknown <aliahmad538404@gmail.com>
2021-04-19 02:53:12 +0500

    Working Scrapper

--- DarazScrapper/views.py
+++ DarazScrapper/views.py
@@ -29,18 +29,19 @@ class Scrap:
             for i in URL:
                 r = requests.get(i)
                 soup = BeautifulSoup(r.text, 'html.parser')
-                scripts = str(soup.find_all("script")[142])
+                scripts = str(soup.find_all("script")[137])
                 stock_s = scripts.split('stockList')[1].strip().split(']')[0][3:]
                 stock_list = json.loads(stock_s)
                 stock = int(stock_list['stoock'])
                 product_name = scripts.split('pdt_name')[1].split('page')[0][2:-2]
-                price = scripts.split('pdt_price')[1].split('"}')[0][6:]
+                price = scripts.split('pdt_price')[1].split('"}')[0][7:]
                 date = datetime.date.today()
                 r = scripts.split('ratings')[4][3:].split('}')[0].split(',')
                 r1 = r[0]+r[1]
                 r2 = scripts.split('ratings')[4][3:].split('}')[0].split(':')[4][1:-1].split(',')
                 r3 = f'Ratings 5-stars:{r2[0]}\t 4-stars:{r2[1]}\t3-stars:{r2[2]}\t2-stars:{r2[3]}\t1-stars:{r2[4]}\t'
                 rating = r1 + '\n' + r3
+
                 discount, discounted_price = Scrap.discount(scripts, price)
                 A = [product_name, stock, price, rating, discount, discounted_price, date]
                 result.append(A)
@@ -50,14 +51,18 @@ class Scrap:
             raise Exception(e)
 
     @classmethod
-    def discount(cls, script,price):
+    def discount(cls, script, price):
         try:
-            stock_s = script.split('pdt_discount')[1].split(',')[0][4:-2]
-            discount = stock_s
-            d = 100 - int(stock_s)
-            discount_price = math.ceil(int(price)*(d/100))
+            p = ''
+            for i in price.split(','):
+                p=p+i
+            stock_s = script.split('pdt_discount')[1].split(',')[0][4:-2].strip('"')
+            discount = int(stock_s)
+            price = int(p)
+            d = 100 - discount
+            discount_price = math.ceil(price*d*0.01)
 
-        except:
+        except Exception as E:
             discount = 0
             discount_price = price
         return discount, discount_price
